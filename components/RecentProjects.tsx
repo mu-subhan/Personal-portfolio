6b6c5a6b27d79599c32f3df5756abbffd87c49cf
Muhammad Subhan <muhammadsubhan8934@gmail.com>
2025-03-07 23:56:54 +0500

    changing

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -65,7 +65,7 @@ const RecentProjects = () => {
 
                 <div className="flex justify-center items-center">
                   <p className="flex lg:text-xl md:text-xs text-sm text-purple">
-                    View Live Site
+                    Visit Live Site
                   </p>
                   <FaLocationArrow className="ms-3" color="#CBACF9" />
                 </div>
